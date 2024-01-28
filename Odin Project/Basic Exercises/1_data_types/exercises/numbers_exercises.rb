b6af2ecca57546abd9924c9279cedf981f37b563
peterlu0ng <peter.luong678@gmail.com>
2024-01-28 18:10:55 +1100

    Basic Exercises Completed

---
+++ Odin Project/Basic Exercises/1_data_types/exercises/numbers_exercises.rb
@@ -0,0 +1,36 @@
+def add(a, b)
+  # return the result of adding a and b
+  a + b
+end
+
+def subtract(a, b)
+  # return the result of subtracting b from a
+end
+
+def multiply(a, b)
+  # return the result of multiplying a times b
+end
+
+def divide(a, b)
+  # return the result of dividing a by b
+end
+
+def remainder(a, b)
+  # return the remainder of dividing a by b using the modulo operator
+end
+
+def float_division(a, b)
+  # return the result of dividing a by b as a float, rather than an integer
+end
+
+def string_to_number(string)
+  # return the result of converting a string into an integer
+end
+
+def even?(number)
+  # return true if the number is even (hint: use integer's even? method)
+end
+
+def odd?(number)
+  # return true if the number is odd (hint: use integer's odd? method)
+end
---
+++ Odin Project/Basic Exercises/9_debugging/exercises/test.rb
@@ -0,0 +1,13 @@
+require 'pry-byebug'
+
+def yell_greeting(string)
+  name = string
+
+  binding.pry
+
+  name = name.upcase
+  greeting = "WASSAP, #{name}!"
+  puts greeting
+end
+
+yell_greeting("bob")
